34dc2df2fcb9338a8d8cdf98e2769cbc425c8c53
Laetitiae <leticia_99@outlook.com.br>
2018-11-08 16:58:47 -0200

    mais js

--- Scripts/funcoes.js
+++ Scripts/funcoes.js
@@ -1,3 +1,19 @@
+
+function downloadOnClick(){
+    printJS('/Resources/smaugo.pdf');
+}
+
+function commentOnClick(){
+    var alerta;
+    var com = prompt("Nós dê um comentario!", "Comentario");
+    if (com == null || com == ""){
+        alerta = "Comentario vazio.";
+    } else {
+        alerta = "Obrigada pelo comentario!";
+    }
+    document.getElementById("buttonRespon").innerHTML = alerta;
+}
+
 var inputVue = new Vue({
     el: '#imgVue',
     data:{
