4373d00f81c9d2a937517dfae08e7996929d3d5b
Laetitiae02 <leticia_99@outlook.com.br>
2018-11-29 15:01:20 -0200

    tentativa de correcao

--- src/comment.php
+++ src/comment.php
@@ -1,13 +1,13 @@
 <?php
-    $nome = $_REQUEST['Nome'];
-    $email = $_REQUEST['Email'];
-    $comentario = $_REQUEST['Comentario'];
+    $nome = $_POST['Nome'];
+    $email = $_POST['Email'];
+    $comentario = $_POST['Comentario'];
 
     $db = new PDO(
         'mysql:host=127.0.0.1;dbname=comment;charset=utf8', 'root', '');
 
     $sql = "INSERT INTO comment".
-            "(nome, email, comentario)".
+            "(nome, email, comment)".
             "VALUES ('$nome', '$email', '$comentario')";
 
     $db->exec($sql);
